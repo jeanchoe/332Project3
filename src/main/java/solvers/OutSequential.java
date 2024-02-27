44f8671bf4541dbe04b8a37a54bd9559e3deff96
Jean Choe <jeanchoe@cs.washington.edu>
2024-02-26 22:25:58 -0800

    checkpoint 1 ver3

--- src/main/java/solvers/OutSequential.java
+++ src/main/java/solvers/OutSequential.java
@@ -1,52 +1,60 @@
 package solvers;
 
-import cse332.exceptions.NotYetImplementedException;
+import cse332.graph.GraphUtil;
 import cse332.interfaces.BellmanFordSolver;
 import main.Parser;
-import java.util.ArrayList;
-import cse332.graph.GraphUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class OutSequential implements BellmanFordSolver {
-    private int[] firstStore;
-    private int[] secondStore;
+    private int[] firstList;
+    private int[] secondList;
     private int[] pointVal;
 
+    @Override
     public List<Integer> solve(int[][] adjMatrix, int source) {
-        List<Map<Integer, Integer>> data = Parser.parse(adjMatrix);
-        firstStore = new int[data.size()];
-        secondStore = new int[data.size()];
-        pointVal = new int[data.size()];
+        ArrayList<Map<Integer, Integer>> graph = Parser.parse(adjMatrix);
+        initialize(graph.size(), source);
 
-        for(int i = 0; i < data.size(); i++){
-            firstStore[i] = Integer.MAX_VALUE;
-            secondStore[i] = Integer.MAX_VALUE;
-            pointVal[i] = -1;
-        }
-        firstStore[source] = 0;
-        secondStore[source] = 0;
-        for(int i = 0; i < data.size(); i++){
-            for(int j = 0; j < data.size(); j++){
-                if(secondStore[j] == Integer.MAX_VALUE) continue;
-                for(int record : data.get(j).keySet()){
-                    int newRecord = data.get(j).get(record);
-                    testValue(record, j, newRecord);
-                    testValue(record, j, newRecord);
-                }
-                for(int k = 0; k < data.size(); k++){
-                    secondStore[k] = firstStore[k];
+        for (int i = 0; i < graph.size(); i++) {
+            for (int j = 0; j < graph.size(); j++) {
+                if (secondList[j] == Integer.MAX_VALUE) continue;
+                for (Map.Entry<Integer, Integer> edge : graph.get(j).entrySet()) {
+                    test(edge.getKey(), j, edge.getValue());
                 }
             }
+            update(graph.size());
         }
+
         return GraphUtil.getCycle(pointVal);
     }
-    private void testValue(int firstData, int i, int secondData){
-        if(firstStore[firstData] > secondStore[secondData]){
-            firstStore[firstData] = secondStore[secondData] + secondData;
-            pointVal[firstData] = i;
+
+    private void initialize(int length, int source) {
+        firstList = new int[length];
+        secondList = new int[length];
+        pointVal = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            firstList[i] = Integer.MAX_VALUE;
+            secondList[i] = Integer.MAX_VALUE;
+            pointVal[i] = -1;
+        }
+        firstList[source] = 0;
+        secondList[source] = 0;
+    }
+
+    private void update(int length) {
+        for (int i = 0; i < length; i++) {
+            secondList[i] = firstList[i];
         }
     }
 
+    private void test(int end, int source, int i) {
+        if (firstList[end] > secondList[source] + i) {
+            firstList[end] = secondList[source] + i;
+            pointVal[end] = source;
+        }
+    }
 }
