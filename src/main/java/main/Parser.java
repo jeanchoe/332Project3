be8a37a6f94018d82980b0a0c954e21ba2ad294d
Jean Choe <jeanchoe@cs.washington.edu>
2024-02-26 22:03:42 -0800

    checkpoint 1

--- src/main/java/main/Parser.java
+++ src/main/java/main/Parser.java
@@ -1,8 +1,9 @@
 package main;
-
 import cse332.exceptions.NotYetImplementedException;
 
-import java.util.List;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
@@ -10,18 +11,31 @@ public class Parser {
     /**
      * Parse an adjacency matrix into an adjacency list.
      * @param adjMatrix Adjacency matrix
-     * @return Adjacency list of maps from node to weight
+     * @return Adjacency list
      */
-    public static List<Map<Integer, Integer>> parse(int[][] adjMatrix) {
-        throw new NotYetImplementedException();
+    private static ArrayList<Map<Integer, Integer>> data;
+    public static ArrayList<Map<Integer, Integer>> parse(int[][] adjMatrix) {
+        data = new ArrayList<>();
+        for(int i = 0; i < adjMatrix[0].length; i++){
+            data.add(i, (new HashMap<>()));
+        }
+        for(int i = 0; i < adjMatrix[0].length; i++){
+            for(int j = 0; j < adjMatrix.length; j++){
+                if(adjMatrix[i][j] != Integer.MAX_VALUE){
+                    data.get(i).put(j,adjMatrix[i][j]);
+                }
+            }
+        }
+        return data;
     }
+
 
     /**
      * Parse an adjacency matrix into an adjacency list with incoming edges instead of outgoing edges.
      * @param adjMatrix Adjacency matrix
      * @return Adjacency list of maps from node to weight with incoming edges
      */
-    public static List<Map<Integer, Integer>> parseInverse(int[][] adjMatrix) {
+    public static ArrayList<Map<Integer, Integer>> parseInverse(int[][] adjMatrix) {
         throw new NotYetImplementedException();
     }
 
--- src/main/java/solvers/OutSequential.java
+++ src/main/java/solvers/OutSequential.java
@@ -3,16 +3,50 @@ package solvers;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.BellmanFordSolver;
 import main.Parser;
+import java.util.ArrayList;
+import cse332.graph.GraphUtil;
 
 import java.util.List;
 import java.util.Map;
 
 public class OutSequential implements BellmanFordSolver {
+    private int[] firstStore;
+    private int[] secondStore;
+    private int[] pointVal;
 
     public List<Integer> solve(int[][] adjMatrix, int source) {
-        List<Map<Integer, Integer>> g = Parser.parse(adjMatrix);
+        List<Map<Integer, Integer>> data = Parser.parse(adjMatrix);
+        firstStore = new int[data.size()];
+        secondStore = new int[data.size()];
+        pointVal = new int[data.size()];
 
-        throw new NotYetImplementedException();
+        for(int i = 0; i < data.size(); i++){
+            firstStore[i] = Integer.MAX_VALUE;
+            secondStore[i] = Integer.MAX_VALUE;
+            pointVal[i] = -1;
+        }
+        firstStore[source] = 0;
+        secondStore[source] = 0;
+        for(int i = 0; i < data.size(); i++){
+            for(int j = 0; j < data.size(); j++){
+                if(secondStore[j] == Integer.MAX_VALUE) continue;
+                for(int record : data.get(j).keySet()){
+                    int newRecord = data.get(j).get(record);
+                    testValue(record, j, newRecord);
+                    testValue(record, j, newRecord);
+                }
+                for(int k = 0; k < data.size(); k++){
+                    secondStore[k] = firstStore[k];
+                }
+            }
+        }
+        return GraphUtil.getCycle(pointVal);
+    }
+    private void testValue(int firstData, int i, int secondData){
+        if(firstStore[firstData] > secondStore[secondData]){
+            firstStore[firstData] = secondStore[secondData] + secondData;
+            pointVal[firstData] = i;
+        }
     }
 
 }
